b121ed059455ff4d0f81d5a5400f642f464170d0
yohanstra <jreinstra@gmail.com>
2015-10-28 11:50:08 -0700

    Added lengths

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 import random
 
 TYPES = (
-    ["C", "C", "C", "C", "C", "B", "L", "T"],
-    ["C", "C", "C", "C", "B", "L", "T"],
+    ["C", "C", "C", "C", "C", "B", "L", "S"],
+    ["C", "C", "C", "C", "B", "L", "S"],
     ["C", "C", "C", "C", "C", "B", "L"],
     ["C", "C", "C", "C", "B", "L"],
 )
+TOTAL_MINI_BLOCKS = 78
+MIN_CLASS_LENGTH = 6
+NUM_CLASS_LENGTHS = 2
+
+MIN_LUNCH_LENGTH = 5
 
 def individual():
-    ordered_types = TYPES[random.randint(0, len(TYPES) - 1)]
-    random.shuffle(ordered_types)
-    return ordered_types
+    shuffled_blocks = TYPES[random.randint(0, len(TYPES) - 1)]
+    random.shuffle(shuffled_blocks)
+    max_length = TOTAL_MINI_BLOCKS / (len(shuffled_blocks) - 2)
+    class_lengths = [random.randint(MIN_CLASS_LENGTH, max_length) for x in range(0, NUM_CLASS_LENGTHS)]
+    
+    schedule = []
+    total_length = 0
+    for block in ordered_types:
+        length = -1
+        if block == "C":
+            length = class_lengths[random.randint(0, NUM_CLASS_LENGTHS - 1)]
+            total_length += length
+        schedule.append((block, length))
+        
+    lunch_length = random.randint(MIN_LUNCH_LENGTH, TOTAL_MINI_BLOCKS - total_class_length - 2)
+    total_length += lunch_length
+    life_length = 
     
     
 def main():
