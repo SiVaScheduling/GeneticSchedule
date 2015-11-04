b3a5206cdd4014f5b6ce24f8b7da07edb69f6d18
yohanstra <jreinstra@gmail.com>
2015-11-04 12:10:07 -0800

    Added documentation

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # GeneticSchedule
-Genetic algorithm for optimizing the Menlo Upper School schedule.
+GeneticSchedule is a simple Python script for optimizing the Menlo School daily schedule. This program does not assign specific classes to specific times; it merely outputs when classes, lunch, break, and student life blocks should occur in the day.
+
+# How it Works
+GeneticSchedule uses a genetic algorithm to generate a large number of random schedules and then evolve these schedules over several generations.  In each generation, only the best-performing individuals survive and reproduce to form the next generation.  Fitness (how well the schedules perform) is determined by measuring the distance between the schedule's parameters (for class length, lunch time, etc) and the ideal parameters, as indivated by a survey sent out to the student body.
+
+# Understanding the Results
+For simplicity and convenience, we used a shorthand notation for describing schedules.  In the example below, you see a schedule that the script may output.
+
+    16 [('C', 15), ('B', 4), ('C', 15), ('C', 15), ('L', 14), ('C', 15)]
+    
+The number on the left is the fitness of the schedule.  This number is fairly arbitrary and is mostly useful only in comparing schedules.  Inside the brackets are six items that look like <strong>('C', 15)</strong>.  These are class blocks.  The first value is a character denoting the type of block. ("C" for class, "B" for break, "S" for student life, and "L" for lunch).  The second value is the length of the class, given in 5-minutes increments.  For example, <strong>('C', 15)</strong> is a 75-minute long class.
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import random
 
+# Evolution parameters
+NUM_INDIVIDUALS = 100000
+NUM_GENERATIONS = 10
+
+# Character for invalid class periods
 INVALID = "X"
 
 # Parameters for schedule generation
@@ -31,13 +36,24 @@ IDEAL_CLASS_LENGTH = 12 # ask colton
 
 
 def individual():
+    """Randomly generates a schedule using the provided constants."""
     shuffled_blocks = TYPES[random.randint(0, len(TYPES) - 1)]
     random.shuffle(shuffled_blocks)
     
+    # Shortest student life block possible
     min_sl_length = MIN_STUDENT_LIFE_LENGTH if "S" in shuffled_blocks else 0
-    max_length = (TOTAL_MINI_BLOCKS - MIN_LUNCH_LENGTH - MIN_BREAK_LENGTH - min_sl_length) / shuffled_blocks.count("C")
-    class_lengths = [random.randint(MIN_CLASS_LENGTH, max_length) for x in range(0, NUM_CLASS_LENGTHS)]
+    # Max length of classes
+    max_length = (
+        TOTAL_MINI_BLOCKS - MIN_LUNCH_LENGTH -
+        MIN_BREAK_LENGTH - min_sl_length
+    ) / shuffled_blocks.count("C")
+    # Generate actual class lengths
+    class_lengths = [
+        random.randint(MIN_CLASS_LENGTH, max_length) for
+        x in range(0, NUM_CLASS_LENGTHS)
+    ]
     
+    # Fill schedule with named blocks and class lengths
     schedule = []
     total_length = 0
     for block in shuffled_blocks:
@@ -47,19 +63,23 @@ def individual():
             total_length += length
         schedule.append((block, length))
     
+    # Generate length of lunch period
     max_lunch_length = TOTAL_MINI_BLOCKS - total_length - MIN_BREAK_LENGTH - min_sl_length
     lunch_length = random.randint(MIN_LUNCH_LENGTH, max_lunch_length) if MIN_LUNCH_LENGTH < max_lunch_length else max_lunch_length
     
     total_length += lunch_length
     
+    # Generate length of break period
     max_break_length = TOTAL_MINI_BLOCKS - total_length - min_sl_length
     break_length = max_break_length if min_sl_length == 0 else random.randint(MIN_BREAK_LENGTH, max_break_length)
     
     total_length += break_length
     
+    # Generate length of student life period
     max_student_life_length = TOTAL_MINI_BLOCKS - total_length
     student_life_length = max_student_life_length
     
+    # Assign break, lunch, and student life lengths to their periods
     for i in range(0, len(schedule)):
         block = schedule[i][0]
         length = schedule[i][1]
@@ -73,8 +93,12 @@ def individual():
         
     return schedule
 
-# lower is better, apparently
+
 def fitness(schedule):
+    """Returns a number describing how well the inputted schedule fits the
+    desired parameters. The closer the score is to 0, the better the inputted
+    schedule fits the parameters.
+    """
     fitness = 0
     
     # Better score for ideal lunch & break time
@@ -99,20 +123,33 @@ def fitness(schedule):
     return fitness
     
 def s_index_of(schedule, name):
+    """Returns the index of the first occurrence of a given period in the
+    given schedule.
+    """
     index = 0
     for block in schedule:
         if block[0] == name:
             return index
         index += block[1]
+        
+    # return -1 if not found
     return -1
 
 def s_value_of(schedule, name):
+    """Returns the value (length) of the first occurrence of a given period in the
+    given schedule.
+    """
     for block in schedule:
         if block[0] == name:
             return block[1]
+        
+    # return -1 if not found
     return -1
 
 def s_values(schedule, name):
+    """Returns a list of the values of a given period in the
+    given schedule.
+    """
     result = []
     for block in schedule:
         if block[0] == name:
@@ -120,6 +157,9 @@ def s_values(schedule, name):
     return result
 
 def s_values_unique(schedule, name):
+    """Returns a list of the unique values of a given period in the
+    given schedule.
+    """
     result = []
     for block in schedule:
         if block[0] == name and block[1] not in result:
@@ -127,6 +167,7 @@ def s_values_unique(schedule, name):
     return result
 
 def s_length(schedule):
+    """Returns the length of the given schedule."""
     index = 0
     for block in schedule:
         index += block[1]
@@ -134,40 +175,57 @@ def s_length(schedule):
         
         
 def population(count):
+    """Generates a list of randomly generated schedules of the
+    given length.
+    """
     return [individual() for x in range(0, count)]
         
         
 def evolve(population, pct_retain=0.2, prob_random_select=0.07, prob_mutate=0.02):
+    """Creates the next generation of a given population with the
+    given parameters.
+    """
+    
+    # list of individuals ordered by fitness
     graded = [(fitness(x), x) for x in population]
     graded = [x[1] for x in sorted(graded)]
     
+    # a portion of the most fit individuals become parents
     parents = graded[:int(len(graded)*pct_retain)]
     
+    # add a random sample from the remaining population to the parents
     for item in population:
         if prob_random_select > random.random():
             parents.append(item)
-            
+        
+    # mutate (generate randomly) a portion of the parents
     for i in range(0, len(parents)):
         if prob_mutate > random.random():
             parents[i] = mate(parents[i], individual())
             
+    # create a population of children by randomly 'mating' parents
     children = []
     for x in range(0, len(population) - len(parents)):
         parent_left = parents[random.randint(0, len(parents) - 1)]
         parent_right = parents[random.randint(0, len(parents) - 1)]
         children.append(mate(parent_left, parent_right))
         
+    # return both the parents and the children for the next generation
     result = parents + children
     return result
 
 
 def mate(left, right):
+    """Combines the two provided schedules into one 'child' schedule."""
+    
+    # Remove duplicates of blocks that can only occur once (lunch, break, etc)
     for name in ONE_BLOCK_MAX:
         if 0.5 > random.random():
             left = s_remove(left, name)
         else:
             right = s_remove(right, name)
     
+    # Add a class from either left or right in each new spot
     result = []
     for i in range(0, min(len(left), len(right))):
         if right[i][0] in ONE_BLOCK_MAX and left[i][0] in ONE_BLOCK_MAX:
@@ -186,8 +244,10 @@ def mate(left, right):
                 next_item = right[i]
 
             if next_item[0] != INVALID:
+                # Only add block if it's valid
                 result.append(next_item)
         
+    # randomly add the last block if it's valid
     if len(left) != len(right):
         last = left[-1:] if len(left) > len(right) else right[-1:]
         if last[0] != INVALID and (last[0] in ONE_BLOCK_MAX or 0.5 > random.random()):
@@ -195,9 +255,10 @@ def mate(left, right):
             
     return result
     
-def s_remove(sched, name):
+def s_remove(schedule, name):
+    """Removes all occurrences of a given period in the given schedule."""
     result = []
-    for item in sched:
+    for item in schedule:
         if item[0] != name:
             result.append(item)
         else:
@@ -206,10 +267,13 @@ def s_remove(sched, name):
     
     
 def main():
-    pop = population(100000)
-    for i in range(1, 10):
+    # Evolve a population of NUM_INDIVIDUALS for NUM_GENERATIONS
+    pop = population(NUM_INDIVIDUALS)
+    for i in range(1, NUM_GENERATIONS):
         print "gen", i
         pop = evolve(pop)
+        
+    # print 10 best schedules
     print "\n\nresults\n"
     for item in [x[1] for x in sorted([(fitness(x), x) for x in pop])][:10]:
         print fitness(item), item
